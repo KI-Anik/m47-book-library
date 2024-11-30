06b64256b342a9fd7bca666b5f807ac9987e9a74
KI-Anik <iamkhairul101@gmail.com>
2024-11-30 11:39:23 +0600

    static nav & foote, 2 children path & errorPage added

---
+++ src/components/Error/Erropage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Erropage = () => {
+    return (
+        <div>
+            <h2 className='text-5xl'>Content Not Found</h2>
+        </div>
+    );
+};
+
+export default Erropage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root';
+import Erropage from './components/Error/Erropage';
+import Home from './components/Home/Home';
+import DashBoard from './components/DashBoard/DashBoard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Erropage></Erropage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/dashboard',
+        element: <DashBoard></DashBoard>
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
